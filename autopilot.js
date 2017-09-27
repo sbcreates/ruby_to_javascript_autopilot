55806da91bb65c76350c8596e0ed0767dca5ca37
sbcreates <drawsarahb@gmail.com>
2017-09-27 15:52:52 -0400

    Converted pick up passenger method to js and tested it works. Added if else statement for when there is one passenger vs multiple passengers

--- autopilot.js
+++ autopilot.js
@@ -22,3 +22,23 @@ function addCar() {
   fleetOfCars.push(getNewCar);
   console.log(`Adding new car to fleet. Fleet size is now ${fleetOfCars.length}`);
 }
+addCar();
+
+// def pick_up_passenger(car)
+//   car[:passengers] += 1
+//   car[:gas] -= 10
+//   "Picked up passenger. Car now has #{car[:passengers]} passengers."
+// end
+
+function pickUpPassenger() {
+  fleetOfCars.forEach(function(car) {
+    car.passengers++;
+    car.gas -= 10;
+    if (car.passengers > 1) {
+    console.log(`Picked up passenger. Car now has ${car.passengers} passengers`);
+  } else {
+    console.log(`Picked up passenger. Car now has ${car.passengers} passenger`);
+  }
+  })
+}
+pickUpPassenger();
